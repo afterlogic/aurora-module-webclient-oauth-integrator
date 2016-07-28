26371890ba4081a668377a25422488c750ce4df6
sash04ek <a.ovcharov@gmail.com>
2016-07-28 15:48:34 +0300

    add Button.html

--- module.php
+++ module.php
@@ -63,6 +63,7 @@ class ExternalServicesModule extends AApiModule
 			{
 				$oAccountOld->setScope('auth');
 				$oAccount->Scopes = $oAccountOld->Scopes;
+				$oAccount->iId = $oAccountOld->iId;
 				$this->oManager->updateAccount($oAccount);
 				
 				$oUser = \CApi::GetModuleDecorator('Core')->GetUser($oAccountOld->IdUser);
