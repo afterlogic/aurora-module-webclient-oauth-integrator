fe08cc12d09e114a24b522960c443724e6bedd0c
nadine-shi <nadine@afterlogic.com>
2016-08-02 15:52:14 +0300

    AdminPanel - split ExternalServices tab into DropBoxAuth, FacebookAuth and GoogleAuth tabs

--- js/views/AdminSettingsView.js
+++ js/views/AdminSettingsView.js
@@ -18,75 +18,13 @@ function CAdminSettingsView()
 	CAbstractSettingsFormView.call(this, Settings.ServerModuleName);
 	
 	/* Editable fields */
-	this.services = ko.observable(this.getConvertedServices());
+	this.services = ko.observable(Settings.Services);
 	/*-- Editable fields */
 }
 
 _.extendOwn(CAdminSettingsView.prototype, CAbstractSettingsFormView.prototype);
 
 CAdminSettingsView.prototype.ViewTemplate = '%ModuleName%_AdminSettingsView';
-
-/**
- * Returns services settings with knockout fields.
- * 
- * @returns {Array}
- */
-CAdminSettingsView.prototype.getConvertedServices = function()
-{
-	var aConvertedServices = [];
-	
-	_.each(Settings.Services, function (oService) {
-		aConvertedServices.push({
-			name: oService.Name,
-			displayName: oService.DisplayName,
-			enable: ko.observable(!!oService.EnableModule),
-			id: ko.observable(oService.Id),
-			secret: ko.observable(oService.Secret)
-		});
-	});
-	
-	return aConvertedServices;
-};
-
-CAdminSettingsView.prototype.getCurrentValues = function()
-{
-	var aValues = [];
-	
-	_.each(this.services(), function (oService) {
-		aValues.push(oService.enable());
-		aValues.push(oService.id());
-		aValues.push(oService.secret());
-	});
-	
-	return aValues;
-};
-
-CAdminSettingsView.prototype.revertGlobalValues = function()
-{
-	this.services(this.getConvertedServices());
-};
-
-CAdminSettingsView.prototype.getParametersForSave = function ()
-{
-	var oParameters = {};
-	
-	_.each(this.services(), function (oService) {
-		oParameters[oService.name] = {
-			'EnableModule': oService.enable(),
-			'Id': oService.id(),
-			'Secret': oService.secret()
-		};
-	});
-	
-	return {
-		'Services': oParameters
-	};
-};
-
-CAdminSettingsView.prototype.applySavedValues = function (oParameters)
-{
-	Settings.updateAdmin(oParameters.Services);
-};
 
 CAdminSettingsView.prototype.setAccessLevel = function (sEntityType, iEntityId)
 {
