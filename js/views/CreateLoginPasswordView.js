bfe98ad6fdd4489d2a19e4d4eb8403414e235e86
nadine-shi <nadine@afterlogic.com>
2016-10-06 16:49:40 +0300

    Accounts - User password setting

--- js/views/CreateLoginPasswordView.js
+++ js/views/CreateLoginPasswordView.js
@@ -4,9 +4,7 @@ var
 	_ = require('underscore'),
 	ko = require('knockout'),
 	
-	Types = require('%PathToCoreWebclientModule%/js/utils/Types.js'),
-	
-	Ajax = require('%PathToCoreWebclientModule%/js/Ajax.js'),
+	Screens = require('%PathToCoreWebclientModule%/js/Screens.js'),
 	App = require('%PathToCoreWebclientModule%/js/App.js'),
 	
 	Settings = require('modules/%ModuleName%/js/Settings.js')
@@ -22,25 +20,30 @@ function CCreateLoginPasswordView()
 	});
 	this.visibleSetPasswordForm = ko.observable(Settings.OnlyPasswordForAccountCreate);
 	this.password = ko.observable('');
-	this.login = ko.observable('');
-	this.loginRequested = ko.observable(false);
-	
-	ko.computed(function () {
-		if (!this.loginRequested() && this.visible() && this.visibleSetPasswordForm())
-		{
-			Ajax.send(Settings.ServerModuleName, 'GetAccounts', null, _.bind(function (oResponse) {
-				if (Types.isNonEmptyArray(oResponse.Result))
-				{
-					this.login(Types.pString(oResponse.Result[0].Email));
-				}
-				this.loginRequested(true);
-				if (this.login() === '')
-				{
-					this.visibleSetPasswordForm(false);
-				}
-			}, this));
-		}
-	}, this).extend({ throttle: 200 });
+	this.passwordFocus = ko.observable(false);
+	this.confirmPassword = ko.observable('');
+	this.confirmPasswordFocus = ko.observable(false);
+	this.login = ko.computed(function () {
+		App.userPublicId();
+	}, this);
+//	this.loginRequested = ko.observable(false);
+//	
+//	ko.computed(function () {
+//		if (!this.loginRequested() && this.visible() && this.visibleSetPasswordForm())
+//		{
+//			Ajax.send(Settings.ServerModuleName, 'GetAccounts', null, _.bind(function (oResponse) {
+//				if (Types.isNonEmptyArray(oResponse.Result))
+//				{
+//					this.login(Types.pString(oResponse.Result[0].Email));
+//				}
+//				this.loginRequested(true);
+//				if (this.login() === '')
+//				{
+//					this.visibleSetPasswordForm(false);
+//				}
+//			}, this));
+//		}
+//	}, this).extend({ throttle: 200 });
 }
 
 CCreateLoginPasswordView.prototype.ViewTemplate = '%ModuleName%_CreateLoginPasswordView';
@@ -50,6 +53,17 @@ CCreateLoginPasswordView.prototype.ViewTemplate = '%ModuleName%_CreateLoginPassw
  */
 CCreateLoginPasswordView.prototype.setPassword = function ()
 {
+	if (this.password() === '')
+	{
+		this.passwordFocus(true);
+		return;
+	}
+	if (this.password() === this.confirmPassword())
+	{
+		Screens.showError(TextUtils.i18n('COREWEBCLIENT/ERROR_PASSWORDS_DO_NOT_MATCH'));
+		this.confirmPasswordFocus(true);
+		return;
+	}
 	App.broadcastEvent(Settings.AuthModuleName + '::CreateUserAuthAccount', {
 		'Login': this.login(),
 		'Password': this.password(),
