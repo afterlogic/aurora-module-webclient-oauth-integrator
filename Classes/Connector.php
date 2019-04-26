61a44ef562ccfe8a54d3f7b556f1f0652feab06f
nadine-shi <nadine@afterlogic.com>
2019-04-26 15:04:35 +0300

    License and other texts improving

--- Classes/Connector.php
+++ Classes/Connector.php
@@ -11,9 +11,6 @@ namespace Aurora\Modules\OAuthIntegratorWebclient\Classes;
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
- *
- * @package Classes
- * @subpackage AuthIntegrator
  */
 class Connector
 {
