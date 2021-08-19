caa9189ea78bc53a9b6c66d8a63fb5e44de3fd9d
Alexandr Kuznetsov <akuz97@yandex.ru>
2021-08-19 18:06:12 +0300

    created link to user for auth account

--- Models/OauthAccount.php
+++ Models/OauthAccount.php
@@ -6,6 +6,10 @@ use Aurora\System\Classes\Model;
 
 class OauthAccount extends Model
 {
+
+	protected $foreignModel = 'Aurora\Modules\Core\Models\User';
+	protected $foreignModelIdColumn = 'IdUser'; // Column that refers to an external table
+
     protected $fillable = [
 		'Id',
 		'IdUser',
