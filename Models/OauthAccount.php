1e87daa7ec27bcd462fc840d0cb95ff853e8888c
sash04ek <a.ovcharov@afterlogic.com>
2021-08-25 16:01:51 +0300

    fixed BUG - can't assign Google account

--- Models/OauthAccount.php
+++ Models/OauthAccount.php
@@ -24,4 +24,69 @@ class OauthAccount extends Model
 		'Disabled',
 		'AccountType'
     ];
+
+	public function getScopesAsArray()
+	{
+		$aResult = array();
+		if (!$this->Disabled)
+		{
+			$aResult = array_map(function($sValue) {
+					if (!empty($sValue))
+					{
+						return strtolower($sValue);
+					}
+				}, explode(' ', $this->Scopes)
+			);
+		}
+
+		return $aResult;
+	}
+
+	/**
+	 * @param string $sScope
+	 *
+	 * @return bool
+	 */
+	public function issetScope($sScope)
+	{
+		return /*'' === $this->Scopes || */false !== strpos(strtolower($this->Scopes), strtolower($sScope));
+	}
+
+	/**
+	 * @param string $sScope
+	 */
+	public function setScope($sScope)
+	{
+		$aScopes = $this->getScopesAsArray();
+		if (!array_search($sScope, array_unique($aScopes)))
+		{
+			$aScopes[] = $sScope;
+			$this->Scopes = implode(' ', array_unique($aScopes));
+		}
+	}
+
+	/**
+	 * @param array $aScopes
+	 */
+	public function setScopes($aScopes)
+	{
+		$this->Scopes = implode(' ', array_unique(array_merge($aScopes, $this->getScopesAsArray())));
+	}
+
+	/**
+	 * @param string $sScope
+	 */
+	public function unsetScope($sScope)
+	{
+		$aScopes = array_map(function($sValue) {
+				return strtolower($sValue);
+			}, explode(' ', $this->Scopes)
+		);
+		$mResult = array_search($sScope, $aScopes);
+		if ($mResult !== false)
+		{
+			unset($aScopes[$mResult]);
+			$this->Scopes = implode(' ', $aScopes);
+		}
+	}
 }
