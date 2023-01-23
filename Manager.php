b8c84e43b1a06772f7ac79bdfac7870a456ad7e3
Vasil Sokolov <vasil@afterlogic.com>
2023-01-23 23:22:03 +0200

    Updated dates in copyright

--- Manager.php
+++ Manager.php
@@ -12,7 +12,7 @@ use Aurora\Modules\OAuthIntegratorWebclient\Models\OauthAccount;
 /**
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
- * @copyright Copyright (c) 2019, Afterlogic Corp.
+ * @copyright Copyright (c) 2023, Afterlogic Corp.
  */
 class Manager extends \Aurora\System\Managers\AbstractManager
 {
--- Module.php
+++ Module.php
@@ -14,7 +14,7 @@ use Aurora\Api;
  *
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
- * @copyright Copyright (c) 2019, Afterlogic Corp.
+ * @copyright Copyright (c) 2023, Afterlogic Corp.
  *
  * @internal
  * @package Modules
